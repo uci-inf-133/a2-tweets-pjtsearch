923892517f78bec1d548800cc57b3ee21d2e16b5
Peter PJT <pjtsearch@protonmail.com>
2025-10-15 13:56:35 -0700

    feat(about): Implement earliest and latest dates

--- js/about.js
+++ js/about.js
@@ -5,13 +5,17 @@ function parseTweets(runkeeper_tweets) {
 		return;
 	}
 
-	tweet_array = runkeeper_tweets.map(function(tweet) {
+	const tweet_array = runkeeper_tweets.map(function (tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('numberTweets').innerText = tweet_array.length;
+
+	const tweetDates = tweet_array.map(t => t.time);
+	document.getElementById("firstDate").innerText = tweetDates.reduce((date, min) => date < min ? date : min).toLocaleDateString()
+	document.getElementById("lastDate").innerText = tweetDates.reduce((date, max) => date > max ? date : max).toLocaleDateString()
 }
 
 //Wait for the DOM to load
