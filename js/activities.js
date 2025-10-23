9f10bd60aa8e8070d4c5fe3b1e59ca30a87e813d
Peter PJT <pjtsearch@protonmail.com>
2025-10-23 12:15:24 -0700

    feat(activities): Add graphs

--- js/activities.js
+++ js/activities.js
@@ -43,8 +43,13 @@ function parseTweets(runkeeper_tweets) {
 			math.mean(posts2.map(p => p.distance)) - math.mean(posts1.map(p => p.distance))
 		)
 		.map(([activity, _]) => activity)
-	document.getElementById("longestActivityType").innerText = activitiesByAvgDistance[0]
-	document.getElementById("shortestActivityType").innerText = activitiesByAvgDistance.at(-1)
+	const threeActivitiesByAvgDistance = activitiesByAvgDistance.filter(a =>
+		a == activitiesByPopularity[0] ||
+		a == activitiesByPopularity[1] ||
+		a == activitiesByPopularity[2]
+	)
+	document.getElementById("longestActivityType").innerText = threeActivitiesByAvgDistance[0]
+	document.getElementById("shortestActivityType").innerText = threeActivitiesByAvgDistance.at(-1)
 
 	const avgWeekendDistance = math.mean(tweet_array
 		.filter(p => p.distance != 0 && (p.time.getDay() == 0 || p.time.getDay() == 6))
@@ -55,6 +60,78 @@ function parseTweets(runkeeper_tweets) {
 		.map(p => p.distance))
 
 	document.getElementById("weekdayOrWeekendLonger").innerText = avgWeekendDistance > avgWeekdayDistance ? "weekends" : "weekdays"
+
+	window.aggregateShown = false
+
+	const activityDistanceSpec = {
+		"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+		"description": "A graph of the distances by day of week for each of the three most popular activities.",
+		"width": 500,
+		"data": {
+			"values": tweet_array
+				.filter(p =>
+					p.activityType == activitiesByPopularity[0] ||
+					p.activityType == activitiesByPopularity[1] ||
+					p.activityType == activitiesByPopularity[2]
+				)
+				.map(p => ({
+					activityType: p.activityType,
+					distance: p.distance,
+					time: p.time
+				}))
+		},
+		"mark": "point",
+		"encoding": {
+			"y": { "field": "distance", "type": "quantitative", "title": "Distance" },
+			"x": { "field": "time", "timeUnit": "day", "type": "ordinal", "title": "Day of the Week" },
+			"color": { "field": "activityType", "type": "nominal" },
+		}
+	};
+	vegaEmbed('#distanceVis', activityDistanceSpec, { actions: false }).then(console.log);
+
+
+	const activityDistanceAggregatedSpec = {
+		"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+		"description": "A graph of the distances by day of week for each of the three most popular activities.",
+		"width": 500,
+		"data": {
+			"values": tweet_array
+				.filter(p =>
+					p.activityType == activitiesByPopularity[0] ||
+					p.activityType == activitiesByPopularity[1] ||
+					p.activityType == activitiesByPopularity[2]
+				)
+				.map(p => ({
+					activityType: p.activityType,
+					distance: p.distance,
+					dayOfWeek: p.time.getDay(),
+				}))
+		},
+		"mark": "point",
+		"encoding": {
+			"y": { "aggregate": "mean", "field": "distance", "type": "quantitative", "title": "Distance" },
+			"x": {
+				"field": "dayOfWeek",
+				"type": "ordinal",
+				"title": "Day of the Week",
+				"axis": { "labelExpr": `(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"])[datum.label]` }
+			},
+			"color": { "field": "activityType", "type": "nominal" },
+		}
+	};
+	document.getElementById("distanceVisAggregated").style = "display: none"
+	vegaEmbed('#distanceVisAggregated', activityDistanceAggregatedSpec, { actions: false });
+
+	document.getElementById("aggregate").addEventListener("click", () => {
+		if (window.aggregateShown) {
+			document.getElementById("distanceVisAggregated").style = "display: none"
+			document.getElementById("distanceVis").style = ""
+		} else {
+			document.getElementById("distanceVis").style = "display: none"
+			document.getElementById("distanceVisAggregated").style = ""
+		}
+		window.aggregateShown = !window.aggregateShown
+	})
 }
 
 //Wait for the DOM to load
